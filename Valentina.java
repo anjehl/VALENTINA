2b6c736d2184e95893c4ceb7fca32fc2912aba39
anjehl <99598259+anjehl@users.noreply.github.com>
2022-02-13 08:41:11 -0500

    Update Valentina.java

--- Valentina.java
+++ Valentina.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Hackathons;
 
-/**
- *
- * @author marya
- */
 import java.util.Scanner;
 
 public class Valentina {
